a3292af9dae13ee5e5b8d32909e9a4421e25b841
Xinjun Ma <xinjun.ma@qq.com>
2021-01-15 21:42:04 +0800

    update gradle

--- src/main/java/org/easysoc/plugins/verilog/paths/FileInfoRefDocumentationProvider.java
+++ src/main/java/org/easysoc/plugins/verilog/paths/FileInfoRefDocumentationProvider.java
@@ -8,7 +8,7 @@ public class FileInfoRefDocumentationProvider extends AbstractDocumentationProvi
     @Override
     public String getQuickNavigateInfo(PsiElement element, PsiElement originalElement) {
         if (element instanceof FileInfoRef.MyFakePsiElement) {
-            return "Jump to Chisel code";
+            return "Jump to the Chisel code";
         }
         return null;
     }
